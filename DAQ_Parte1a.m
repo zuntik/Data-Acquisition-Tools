7372478d8a740a614b2d0c536e1ce7dbcd758039
thomas <thomasdpb@gmail.com>
2018-04-23 11:13:51 +0100

    more more

--- DAQ_Parte1a.m
+++ DAQ_Parte1a.m
@@ -23,11 +23,31 @@ size(transverse(S(:,1))
 
 %% situação 1
 
+% sinusoide com amplitude 2V  freq = 2.5kHz  ; 
+N = 400 ; 
+Fs = 50e3;
+
+
+
+s1 = daq.createSession( 'ni');
+s1.addAnalogInputChannel('Dev1','ai0','Voltage');
+s1.Rate = Fs;
+s1.DurationInSeconds = N / fs;
+
+S = startForeground(s1);
+
+[ freqest, valef, valmed, freqs, ssps ] = signalinfo( valores , fs )
+
+plot(s1);
+
+
 
 fig1 = subplot(2,1,1);
 
 plot(1)
-title(fig1, 
+title(fig1, {'Situação 1 - ';'Freq estimada ';  sprintf('%f',freqest); sprintf('%f',freqest); '; Va' });
+
+
 
 
 
